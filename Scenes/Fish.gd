3ba25c4e2af22399aeb886bd840c374b5700b15b
User <User@Gm16.local>
2020-09-08 14:53:41 +0700

    Added fish

--- Scenes/Fish.gd
+++ Scenes/Fish.gd
@@ -1,30 +1,25 @@
 extends KinematicBody
 
-const SPEED = 0.5
+export (float) var SPEED = 2
+export (float) var VIEW  = 10
 
 var beckon_pos = Vector3(0,0,0)
+var direction = Vector3(0,0,0)
 
 func _ready():
-	randomize()
-	
-	beckon_pos.x = rand_range(-5, 5)
-	beckon_pos.z = rand_range(-5, 5)
-
-func beckon(point):
-
-	if translation.x > point.x:
-		move_and_slide(Vector3(SPEED*-1,0,0))
-	elif translation.x < point.x:
-		move_and_slide(Vector3(SPEED,0,0))
-
-	if translation.z > point.z:
-		move_and_slide(Vector3(0,0,SPEED*-1))
-	elif translation.z < point.z:
-		move_and_slide(Vector3(0,0,SPEED))
+	$AnimationPlayer.play("Fish")
 
 func _physics_process(delta):
-	if translation.distance_to(beckon_pos) > 0.5:
-		beckon(beckon_pos)
+	if translation.distance_to(beckon_pos) > SPEED*2:
+		beckon(direction)
 	else:
-		beckon_pos.x = rand_range(-5, 5)
-		beckon_pos.z = rand_range(-5, 5)
+		randomPoint()
+		
+func beckon(point):
+	move_and_slide(point)
+		
+func randomPoint():
+	beckon_pos.x = rand_range(VIEW*-1, VIEW)
+	beckon_pos.z = rand_range(VIEW*-1, VIEW)
+	direction = (beckon_pos-translation).normalized() * SPEED
+	look_at(beckon_pos, Vector3(0, 1, 0))
